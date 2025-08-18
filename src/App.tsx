3285a5614dc022969ad0c5b926d8ed5c50c6a234
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2025-08-18 21:18:08 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GameRoom from './pages/GameRoom';
+import TicTacToeRoom from './pages/TicTacToeRoom';  // صفحة إكس أو
+import Index from './pages/Index';  // الصفحة الرئيسية
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import GameRoom from "./pages/GameRoom";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/play" element={<GameRoom />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/play" element={<GameRoom />} />
+        <Route path="/tic-tac-toe" element={<TicTacToeRoom />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
