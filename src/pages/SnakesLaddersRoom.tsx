25715798aa854fdde8f93e0c516632b70594becb
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2025-08-20 04:47:57 +0300

    Update SnakesLaddersRoom.tsx

--- src/pages/SnakesLaddersRoom.tsx
+++ src/pages/SnakesLaddersRoom.tsx
@@ -514,7 +514,7 @@ const SnakesLaddersRoom = () => {
                     
                     // حساب الإحداثيات بناءً على الصف والعمود
                     const adjustedRowIndex = 9 - rowIndex; 
-                    const top = `${adjustedRowIndex * 10}%`;
+                    const top = `${adjustedRowIndex * 7}%`;
                     const left = `${colIndex * 10}%`;
                     const width = '10%';
                     const height = '10%';
