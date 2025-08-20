904553af89ffb4c0b5441e408a2e2ae5f94580e2
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2025-08-20 05:09:27 +0300

    Update SnakesLaddersRoom.tsx

--- src/pages/SnakesLaddersRoom.tsx
+++ src/pages/SnakesLaddersRoom.tsx
@@ -493,12 +493,12 @@ const SnakesLaddersRoom = () => {
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="relative mb-3 mx-5" style={{ maxWidth: '400px' }}>
+            <div className="relative mb-4 mx-auto" style={{ maxWidth: '400px' }}>
               {/* خلفية اللوحة */}
               <img 
                 src="/snakes-ladders-board.jpg" 
                 alt="لوحة السلم والثعبان" 
-                className="w-full h-auto rounded-lg shadow-inner"
+                className="w-full h-5 rounded-lg shadow-inner"
               />
               
               {/* شبكة الخلايا الشفافة فوق الخلفية */}
